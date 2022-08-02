cc9ee94033122fdf70fe17183ea7a9882dd0df2c
jane-zizhen-zhao <jane.zizhen.zhao@gmail.com>
2022-08-02 09:10:46 -0500

    Initial Commit

---
+++ dear beautiful human/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dear beautiful human
+//
+//  Created by scholar on 7/31/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ dear beautiful human/signInViewController.swift
@@ -0,0 +1,29 @@
+//
+//  signInViewController.swift
+//  dear beautiful human
+//
+//  Created by scholar on 7/31/22.
+//
+
+import UIKit
+
+class signInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
