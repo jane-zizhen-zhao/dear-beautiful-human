004b9d8a07fac514b49a7557ae77ac6f94f70e1a
Jane Zhao <jane.zizhen.zhao@gmail.com>
2022-08-03 09:44:12 -0500

    test run

---
+++ dear beautiful human/createGoalViewController.swift
@@ -0,0 +1,31 @@
+//
+//  createGoalViewController.swift
+//  dear beautiful human
+//
+//  Created by scholar on 8/2/22.
+//
+
+import UIKit
+
+class createGoalViewController: UIViewController {
+
+    @IBOutlet weak var whatIsYourGoal: UITextField!
+    
+    
+    @IBOutlet weak var date: UISwitch!
+    
+    @IBOutlet weak var time: UISwitch!
+    
+    
+    @IBOutlet weak var `repeat`: UISwitch!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func setGoalTapped(_ sender: Any) {
+        }
+    }
+    
---
+++ dear beautiful human/simpleGoals.swift
@@ -0,0 +1,15 @@
+//
+//  simpleGoals.swift
+//  dear beautiful human
+//
+//  Created by scholar on 8/2/22.
+//
+
+import UIKit
+
+class SimpleGoals {
+    var goal = ""
+    var date = true
+    var time = true
+    var repetitive = true
+}
