170088ba0f41f6089be42e2cad107a7e98b172cc
Ashley Wong <ashleywch10@gmail.com>
2022-08-04 09:17:03 -0700

    emoji update

--- dear beautiful human/ViewController.swift
+++ dear beautiful human/ViewController.swift
@@ -11,9 +11,39 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
+   
     }
-
-
+    
+    @IBAction func smileButton(_ sender: Any) {
+    }
+    /*@IBAction func smileButton(_ sender: UIButton) {
+        func hiliteButtons ()
+        {
+            smileButton.backgroundColor = UIColor(red: 1.0, green: 0.5, blue: 1.0, alpha: 1.0)
+            if smileButton.backgroundColor == UIColor(red: 1.0, green: 0.5, blue: 1.0, alpha: 1.0){
+                smileButton.backgroundColor = UIColor(red: 0.5, green: 1.0, blue: 0.5, alpha: 1.0)
+            }
+            else if smileButton.backgroundColor == UIColor(red: 0.5, green: 1.0, blue: 0.5, alpha: 1.0)
+            {
+                smileButton.backgroundColor = UIColor(red: 1.0, green: 0.5, blue: 1.0, alpha: 1.0)
+            }
+        
+            }
+        let button = UIButton(type: .system)
+        button.configuration = .filled()
+        var config = UIButton.Configuration.filled()
+        config.title = "Custom Filled Button"
+        button.configuration = config
+        let smileButton = UIButton(configuration: config,primaryAction: UIAction() { _ in print("Go")
+            
+        })*/
+    
 }
+        
+    
+
+
+
 
--- dear beautiful human/homeScreenViewController.swift
+++ dear beautiful human/homeScreenViewController.swift
@@ -8,14 +8,81 @@
 import UIKit
 
 class homeScreenViewController: UIViewController {
-
+    var button_isActive: Bool = false
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func smileButton(_ sender: UIButton) {
+        if button_isActive {
+               UIView.animate(withDuration: 0.2) {
+                   sender.backgroundColor = UIColor.clear
+               }
+           } else {
+               UIView.animate(withDuration: 0.2) {
+                   sender.backgroundColor = UIColor.purple
+               }
+           }
+           button_isActive = !button_isActive
+       }
+    
+    @IBAction func partyButton(_ sender: UIButton) {
+        if button_isActive {
+               UIView.animate(withDuration: 0.2) {
+                   sender.backgroundColor = UIColor.clear
+               }
+           } else {
+               UIView.animate(withDuration: 0.2) {
+                   sender.backgroundColor = UIColor.purple
+               }
+           }
+           button_isActive = !button_isActive
+    }
+    
+    @IBAction func sleepButton(_ sender: UIButton) {
+        if button_isActive {
+               UIView.animate(withDuration: 0.2) {
+                   sender.backgroundColor = UIColor.clear
+               }
+           } else {
+               UIView.animate(withDuration: 0.2) {
+                   sender.backgroundColor = UIColor.purple
+               }
+           }
+           button_isActive = !button_isActive
+    }
+    
+    @IBAction func sadButton(_ sender: UIButton) {
+        if button_isActive {
+               UIView.animate(withDuration: 0.2) {
+                   sender.backgroundColor = UIColor.clear
+               }
+           } else {
+               UIView.animate(withDuration: 0.2) {
+                   sender.backgroundColor = UIColor.purple
+               }
+           }
+           button_isActive = !button_isActive
+    }
+    
+    @IBAction func madButton(_ sender: UIButton) {
+        if button_isActive {
+               UIView.animate(withDuration: 0.2) {
+                   sender.backgroundColor = UIColor.clear
+               }
+           } else {
+               UIView.animate(withDuration: 0.2) {
+                   sender.backgroundColor = UIColor.purple
+               }
+           }
+           button_isActive = !button_isActive
+    }
+    
+}
+    
+    
     /*
     // MARK: - Navigation
 
@@ -26,4 +93,3 @@ class homeScreenViewController: UIViewController {
     }
     */
 
-}
